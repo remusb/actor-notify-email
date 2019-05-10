75027d7f27a05243763df8f7ba3fb2c19f2f3ad6
Remus Bunduc <bunduc@adobe.com>
2019-05-10 11:11:55 +0300

    refactor

--- main.js
+++ main.js
@@ -7,23 +7,22 @@ Apify.main(async () => {
     const htmlOutTemplate = fs.readFileSync('/template/email.html', 'utf8');
     let cnt = 0;
 
-    const ter = await Apify.openKeyValueStore('terenuri');
-    const cas = await Apify.openKeyValueStore('case');
+    const store = await Apify.openKeyValueStore(process.env.NOTIFY_TYPE);
 
-    if (ter != null) {
+    if (store != null) {
         let htmlOut = htmlOutTemplate;
         let executariOut = '';
         let vanzareOut = '';
 
         let entryKeys = [];
-        await ter.forEachKey(async (key, index, info) => {
+        await store.forEachKey(async (key, index, info) => {
             entryKeys.push(key);
         });
 
         console.log(`Processing ${entryKeys.length} entries`);
         for (let i = 0; i < entryKeys.length; i++) {
             const key = entryKeys[i];
-            const entry = await ter.getValue(key);
+            const entry = await store.getValue(key);
 
             if (('notified' in entry) && entry.notified && process.env.INCLUDE_NOTIFIED != "1") {
                 continue;
@@ -31,7 +30,7 @@ Apify.main(async () => {
             if (!('title' in entry)) {
                 console.log(`Ignoring entry: ${JSON.stringify(entry)}`);
                 if (process.env.DELETE_INVALID == "1") {
-                    await ter.setValue(key, null);
+                    await store.setValue(key, null);
                 }
                 continue;
             }
@@ -63,22 +62,30 @@ Apify.main(async () => {
 
             if (process.env.INCLUDE_NOTIFIED != "1") {
                 entry.notified = true;
-                await ter.setValue(key, entry);
+                await store.setValue(key, entry);
             }
 
             if (entry.executare) {
                 executariOut += `<tr><td>${entry.title}, ${entry.size} mp</td><td>${entry.price} EUR</td><td>${entry.detail}</td>`;
                 executariOut += `<td><a href="${entry.url}" target="_blank">Link</a></td></tr>`;
             } else {
-                vanzareOut += `<tr><td>${entry.title}, ${entry.size} mp</td><td>${entry.price} EUR</td><td>${entry.detail}</td>`;
+                if (process.env.NOTIFY_TYPE == 'terenuri') {
+                    vanzareOut += `<tr><td>${entry.title}, ${entry.size} mp</td><td>${entry.price} EUR</td><td>${entry.detail}</td>`;
+                } else {
+                    vanzareOut += `<tr><td>${entry.title}</td><td>${entry.house}, ${entry.size}, ${entry.rooms} cam.</td><td>${entry.price} EUR</td><td>${entry.detail}</td>`;
+                }
                 vanzareOut += `<td><a href="${entry.url}" target="_blank">Link</a></td></tr>`;
             }
             cnt++;
         }
 
         if (executariOut != '' || vanzareOut != '') {
-            htmlOut += '<h1>Vanzari</h1>' + '<table><thead><tr><th>Titlu/Supr.</th><th>Pret</th><th>Detalii</th><th>URL</th></thead>' + vanzareOut + '</table>';
-            htmlOut += '<h1>Executari</h1>' + '<table><thead><tr><th>Titlu/Supr.</th><th>Pret</th><th>Detalii</th><th>URL</th></thead>' + executariOut + '</table>';
+            if (process.env.NOTIFY_TYPE == 'terenuri') {
+                htmlOut += '<h1>Vanzari</h1>' + '<table><thead><tr><th>Titlu/Supr.</th><th>Pret</th><th>Detalii</th><th>URL</th></thead>' + vanzareOut + '</table>';
+                htmlOut += '<h1>Executari</h1>' + '<table><thead><tr><th>Titlu/Supr.</th><th>Pret</th><th>Detalii</th><th>URL</th></thead>' + executariOut + '</table>';
+            } else {
+                htmlOut += '<h1>Vanzari</h1>' + '<table><thead><tr><th>Titlu</th><th>Supr/Cam</th><th>Pret</th><th>Detalii</th><th>URL</th></thead>' + vanzareOut + '</table>';
+            }
             htmlOut += '</body></html>';
 
             console.log(`Sending e-mail using ${process.env.GMAIL_USER}`);
@@ -92,15 +99,15 @@ Apify.main(async () => {
                     pass: process.env.GMAIL_PASSWORD
                 }
             });
-            let emailTitle = `Status Terenuri: ${cnt}`;
+            let emailTitle = `Status ${process.env.NOTIFY_TYPE}: ${cnt}`;
             if ('CONTAINS' in process.env) {
-                emailTitle = `Status Terenuri / ${process.env.CONTAINS}: ${cnt}`;
+                emailTitle = `Status ${process.env.NOTIFY_TYPE} / ${process.env.CONTAINS}: ${cnt}`;
             }
             if ('SECTOR' in process.env) {
-                emailTitle = `Status Terenuri / Sector ${process.env.SECTOR}: ${cnt}`;
+                emailTitle = `Status ${process.env.NOTIFY_TYPE} / Sector ${process.env.SECTOR}: ${cnt}`;
             }
             if ('AD_TYPE' in process.env) {
-                emailTitle = `Status Terenuri / ${process.env.AD_TYPE.charAt(0).toUpperCase() + process.env.AD_TYPE.slice(1)}: ${cnt}`;
+                emailTitle = `Status ${process.env.NOTIFY_TYPE} / ${process.env.AD_TYPE.charAt(0).toUpperCase() + process.env.AD_TYPE.slice(1)}: ${cnt}`;
             }
             const info = await transporter.sendMail({
                 from: process.env.GMAIL_USER,
@@ -109,8 +116,6 @@ Apify.main(async () => {
                 html: htmlOut
             });
             console.log(`Info mail: ${info}`);
-
-            await Apify.setValue('OUTPUT_TERENURI', htmlOut);
         }
     }
 
